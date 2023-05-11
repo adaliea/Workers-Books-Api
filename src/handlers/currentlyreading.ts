b5335e78a194ee4166b14c253f89279e61510735
varun7654 <varun7654@outlook.com>
2023-05-10 19:32:22 -0700

    add Access-Control-Allow-Origin policy

--- src/handlers/currentlyreading.ts
+++ src/handlers/currentlyreading.ts
@@ -8,9 +8,9 @@ const host = 'https://openlibrary.org';
 const url = host + '/people/dacubeking5259/books/currently-reading.json';
 
 class Book {
-	name!: string
-	link!: string
-  author!: string
+	name!: string;
+	link!: string;
+	author!: string;
 }
 
 /**
@@ -27,22 +27,22 @@ async function gatherResponse(response) {
 	return response.text();
 }
 
-function combineList(list : string[]) {
-  var out = ""
+function combineList(list: string[]) {
+	var out = '';
 
-  // combine all of the elements into a nicely formatted string with commas and an "&"
-  // be sure not to add "&" if there is only one element in the list
-  for (var i = 0; i < list.length; i++) {
-    if (i == 0) {
-      out += list[i]
-    } else if (i == list.length - 1) {
-      out += " & " + list[i]
-    } else {
-      out += ", " + list[i]
-    }
-  }
+	// combine all of the elements into a nicely formatted string with commas and an "&"
+	// be sure not to add "&" if there is only one element in the list
+	for (var i = 0; i < list.length; i++) {
+		if (i == 0) {
+			out += list[i];
+		} else if (i == list.length - 1) {
+			out += ' & ' + list[i];
+		} else {
+			out += ', ' + list[i];
+		}
+	}
 
-  return out
+	return out;
 }
 
 const CurrentlyReading = async request => {
@@ -52,20 +52,25 @@ const CurrentlyReading = async request => {
 
 	let books: Book[] = [];
 
+	json.reading_log_entries.forEach(
+		(bookJson: { work: { title: any; key: any; author_names: string[] } }) => {
+			books.push({
+				name: bookJson.work.title,
+				link: host + bookJson.work.key,
+				author: combineList(bookJson.work.author_names),
+			});
+		}
+	);
 
-	json.reading_log_entries.forEach((bookJson: { work: { title: any;  key: any; author_names: string[]; }; }) => {
-    books.push(
-       {
-        name : bookJson.work.title,
-        link : host + bookJson.work.key,
-        author : combineList(bookJson.work.author_names)
-       }
-    )
-	});
-
-  console.log(books)
-	const body = combineList(books.map(book => `<a href="${book.link}">${book.name} by ${book.author}</a>`));
-	const headers = { 'Content-type': 'text/html' };
+	console.log(books);
+	const body = combineList(
+		books.map(book => `<a href="${book.link}">${book.name} by ${book.author}</a>`)
+	);
+  
+	const headers = {
+		'Access-Control-Allow-Origin': '*',
+		'Content-type': 'text/html',
+	};
 	return new Response(body, { headers });
 };
 
